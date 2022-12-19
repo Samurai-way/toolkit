8200235c74b4f47ef981dd47ccb67712854663b6
Samurai-way <olegmoishevych@gmail.com>
2022-12-19 22:45:03 +0200

    learn: redux-toolkit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {useAppDispatch, useAppSelector} from "./hooks/redux";
+import {userSlice} from "./store/reducers/userSlice";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const {count} = useAppSelector(state => state.userReducer)
+
+    const increment = userSlice.actions.increment
+    const decrement = userSlice.actions.decrement
+
+    const dispatch = useAppDispatch()
+
+    const inc = () => {
+        dispatch(increment(+1))
+    }
+
+    const dec = () => {
+        dispatch(decrement(count - 1))
+    }
+    return (
+        <div className="App">
+            <h1>{count}</h1>
+            <button onClick={inc}>inc</button>
+            <button onClick={dec}>dec</button>
+        </div>
+    );
 }
 
 export default App;
